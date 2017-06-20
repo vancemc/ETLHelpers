3dd81617506ea9685e096c30a5c863ad758a1fce
Vance McCorkle <vance.mccorkle@providence.org>
2017-06-20 12:49:57 -0700

    Added comments and bug fixes

--- CreateEtlEnvironmentConfigs.ps1
+++ CreateEtlEnvironmentConfigs.ps1
@@ -1,16 +1,32 @@
 ﻿# This script runs in Windows integrated authentication.
 # Make sure script is running under an account with privileges to access target sql database instance
 
-$maxResultLength = 30000 #max characters defined for Invoke-Sqlcmd which will truncate result at 4k otherwise
+#max characters defined for Invoke-Sqlcmd which will truncate result at 4k otherwise
+$maxResultLength = 30000 
 
-$server = "AMTST46SQL05.az.fox.afg"
+#set the source and target servers 
 
+#sourceServer = where we get the environment configuration from
+$sourceServer = "AMTST46SQL05.az.fox.afg"
+
+#targetServer = the target SQL instance where we want to re-create the environment
+#               configuration settings.
+$targetServer = "localhost"
+
+#$folder = The name of the SSISDB child folder name.
+#          Only use folder name one level deep from the parent SSISDB node.
+#          e.g. for EPS\Projects\enrollment-processing the folder to use would be EPS
 $folder = "IssuerDisputesETL"
+
+#env = the environment configuration source and target name
 $env = "TEST"
 
+#$CreateEnvQuery = the query to the stored procedure that will generate he environment
+#                  configuration script to be run on the target SQL instance
 $CreateEnvQuery = 
 
 "
+USE master
 DECLARE	@return_value int,
 		@script varchar(max)
 
@@ -24,8 +40,17 @@ SELECT	@script as N'@script'
 SELECT	'Return Value' = @return_value
 "
 
-$result = Invoke-Sqlcmd -ServerInstance AMTST46SQL05.az.fox.afg -Database DisputesDb -Query $CreateEnvQuery -MaxCharLength $maxResultLength
+#Invoke the storedproc on the source SQL instance
+$result = Invoke-Sqlcmd -ServerInstance $sourceServer -Database DisputesDb -Query $CreateEnvQuery -MaxCharLength $maxResultLength
 
-Write-Host $result.script
+#Convert $result.script which is an object[] array into a proper string
+$createEnvScript = $result.script | Out-String
 
+#write the environment creation script to the console for debugging purposes
+Write-Host $createEnvScript
+
+#save the environment creation script if needed
 $result.script | Out-File c:\scripts\$folder.sql
+
+#run the environment creation script on the target machine to create the environment configuration pulled from the source server
+Invoke-Sqlcmd -ServerInstance $targetServer -Database Master -Query $createEnvScript -MaxCharLength $maxResultLength
